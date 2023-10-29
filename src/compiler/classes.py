a0bd9cafe1ab09ed281e66429384b38060d2fd89
vicous6 <vicous@free.fr>
2023-10-29 19:26:07 +0100

    add classes

---
+++ src/compiler/classes.py
@@ -0,0 +1,164 @@
+from abc import ABC, abstractmethod
+
+
+class TokenType:
+    ID = "ID"
+    OP = "OP"
+    NUM = "NUM"
+    EOL = "EOL"
+
+
+class Token:
+    def __init__(self, _type, _value):
+        self.type = _type
+        self.value = _value
+
+    def __str__(self):
+        return "TOKEN : Type = " + str(self.type) + " ; Value = " + str(self.value)
+
+
+class Instruction(ABC):
+    def toString(self, offset):
+        indentation = " " * (4 * offset)
+        inner = "    "
+        newline = "\n" + indentation
+        output = "{" + newline
+
+        for key, value in self.__dict__.items():
+            svalue = value
+            if isinstance(value, Instruction):
+                svalue = f"{value.toString(offset + 1)}"
+
+            output += inner + f'"{key}": {svalue}' + newline
+
+        output += "}"
+
+        return output
+
+    @abstractmethod
+    def transpile(self):
+        return ""
+
+
+class LiteralNumber(Instruction):
+    def __init__(self, value: int):
+        self.value = value
+
+    def transpile(self):
+        return str(self.value)
+
+
+class VarReading(Instruction):
+    def __init__(self, name: str):
+        self.name = name
+
+    def transpile(self):
+        return self.name
+
+
+class BinaryOperation(Instruction):
+    def __init__(self,
+                 operator: str,
+                 a: 'LiteralNumber | VarReading | BinaryOperation',
+                 b: 'LiteralNumber | VarReading | BinaryOperation'):
+        self.operator = operator
+        self.a = a
+        self.b = b
+
+    def transpile(self):
+        return f"{self.a.transpile()}{self.operator}{self.b.transpile()}"
+
+
+class VarAssignation(Instruction):
+    def __init__(self, name: str, value: LiteralNumber | VarReading | BinaryOperation):
+        self.name = name
+        self.value = value
+
+    def transpile(self):
+        return f"{self.name}={self.value.transpile()};"
+
+
+class VarDeclaration(Instruction):
+    def __init__(self, name: str, value: LiteralNumber | VarReading | BinaryOperation):
+        self.name = name
+        self.value = value
+
+    def transpile(self):
+        return f"int {self.name}={self.value.transpile()};"
+
+
+class NativeFunctionCall(Instruction):
+    def __init__(self, name: str, *parameters: LiteralNumber | VarReading | BinaryOperation):
+        self.name = name
+        self.parameters = parameters
+
+    def transpile(self):
+        sparams = ""
+
+        for param in self.parameters:
+            sparams += param.transpile() + ","
+
+        return f'{self.name}({sparams[0:-1]});'
+
+
+class FunctionPrint(NativeFunctionCall):
+    def __init__(self, *parameters: LiteralNumber | VarReading | BinaryOperation):
+        super().__init__("printf", *parameters)
+
+    def transpile(self):
+        sparams = ""
+
+        for param in self.parameters:
+            sparams += param.transpile() + ","
+
+        return f'{self.name}("%i",{sparams[0:-1]});'
+
+
+class AbstractSyntaxTree:
+    def __init__(self, *instructions: Instruction):
+        self.instructions = list(instructions)
+
+    def __str__(self):
+        output = '{\n    "program": [\n        '
+
+        for instruction in self.instructions:
+            output += instruction.toString(2) + ",\n        "
+
+        return output[0:-10] + "\n    ]\n}"
+
+    def addInstruction(self, *instructions: Instruction):
+        self.instructions += instructions
+
+    def transpile(self):
+        output = "#include <stdio.h>\nint main(){"
+
+        for instruction in self.instructions:
+            output += instruction.transpile()
+
+        return output + "}"
+
+
+a = BinaryOperation("-", LiteralNumber(5), LiteralNumber(1))
+b = BinaryOperation("+", a, LiteralNumber(9))
+t = VarDeclaration("a", b)
+
+f = BinaryOperation("-", LiteralNumber(1), LiteralNumber(1))
+h = BinaryOperation("+", f, LiteralNumber(9))
+
+q = FunctionPrint(h)
+
+g = AbstractSyntaxTree(t, q)
+# print(g.transpile())
+
+"""
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    int c = a + b + 1;
+    c = 5 + c * 5;
+    printf("%i",c);
+}
+"""
--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -178,11 +178,15 @@ def scan(start, line):
         currentEl = line[start]
         nextEl = line[start + 1]
         # print(currentEl)
-
+        # print(NATIVES)
+        # print(vartab)
+        # print(currentEl)
+        # print(isIdNative(currentEl))
         # check si pas 2 "="
         if currentEl.value == "=" :
             equalCount += 1
         if equalCount == 2: return "Error Syntax deux signes '="
+
 
 
         # si deux element identiques a la suite
@@ -196,7 +200,11 @@ def scan(start, line):
         #  si la fonction native n'est pas en premiere place
         elif ((currentEl.type == TokenType.ID and isIdNative(currentEl) and start != 0) or
               (nextEl.type == TokenType.ID and isIdNative(nextEl) and start != 0)):
-            return "Error Syntax: a native function must be in 1st position"
+            return "Error Syntax: A native function must be in 1st position"
+        #  si la fonction native est en première place elle DOIT etre suivi de :
+        elif (currentEl.type == TokenType.ID and isIdNative(currentEl) and not nextEl.value ==":"):
+
+            return "Error Syntax: A native function must be follow by ':'"
 
         # si la varible n'existe pas dans vartab et n'est pas en 1ere position
         elif currentEl.type == TokenType.ID and isIdNative(currentEl) == False and isVarNameAvailable(currentEl ,vartab)== False and start!= 0:
@@ -204,7 +212,7 @@ def scan(start, line):
             return "Error Syntax: la varible '" + currentEl.value+"' n'est pas defini"
 
         # si la variable est en position 1 elle doit etre suivi d'un "=" (asignation ou delcaration)
-        elif currentEl.type == TokenType.ID and  nextEl.value != "=" and start ==0:
+        elif currentEl.type == TokenType.ID and not isIdNative(currentEl) and  nextEl.value != "=" and start ==0:
             return "Error Syntax: la varible '" + currentEl.value + "' n'est pas suivi d'un '='"
 
 
@@ -214,12 +222,12 @@ def scan(start, line):
 
 
         # si le token est un un nombre , il DOIT etre suivis d' OP(not : et =) ou de EOL
-        elif not nextEl.value == "=" and not nextEl.value == ":" and currentEl.type == TokenType.NUM and not (nextEl.type == TokenType.OP or nextEl.type == TokenType.EOL ):
+        elif start == 0 and not nextEl.value == "=" and not nextEl.value == ":" and currentEl.type == TokenType.NUM and not (nextEl.type == TokenType.OP or nextEl.type == TokenType.EOL ):
             return "Error Syntax: le nombre '" + currentEl.value + "' est suivi d'un caractère incorect"
 
         #   si la variable est un operateur elle DOIT etre suvis d'une variable ou d'un nombre
         elif currentEl.type == TokenType.OP and not (nextEl.type == TokenType.ID or nextEl.type == TokenType.NUM ):
-            return ("Error Syntax: le nombre '" + currentEl.value + "' est suivi d'un caractère incorect")
+            return ("Error Syntax: l'operateur '" + currentEl.value + "' est suivi d'un caractère incorect")
         else:
             start += 1
             return scan(start, line)
@@ -231,7 +239,7 @@ def scan(start, line):
 
             varId+=1
             vartab[line[0].value] = varId
-            #
+
 
 
     return True
@@ -246,21 +254,7 @@ def parser(lines):
         res = scan(0, line)
         if(res != True):
             errortab.append(res)
-            print("Line "+ str(i) +": "+res)
+            print("Line "+ str(i+1) +": "+res)
         else:
-            print("Line " + str(i) + ": True ")
+            print("Line " + str(i+1) + ": True ")
 
-        # if name == "id":
-        #     pass
-        #
-        # elif name == "op":
-        #     pass
-        #
-        # elif name == "num":
-        #     pass
-        #
-        # elif name == "eol":
-        #     pass
-        #
-        # else:
-        #     print("wtf whats happening: invalid token")
