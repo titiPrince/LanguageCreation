c529c1579b83d05c9774ff2167118ad69adf1acd
vicous6 <vicous@free.fr>
2023-10-30 09:38:53 +0100

    try to transpile but fail

--- src/compiler/transpiler.py
+++ src/compiler/transpiler.py
@@ -18,26 +18,52 @@ def transpile(lines):
     for line in lines:
         line.reverse()
         for i, token in enumerate(line):
-            if token.value == Symbol.ASSIGN: break
+            # if token.value == Symbol.ASSIGN: break
             if token.type != TokenType.OP: continue
 
             prevToken = line[i-1]
             nextToken = line[i+1]
-
+            curentToken = line[i]
+            lastOperation=""
             if token.value in Symbol.OPERATIONS:
                 print(prevToken)
-                if prevToken.type == TokenType.NUM:
-                    a = LiteralNumber(prevToken.value)
-                else:
-                    a = VarReading(prevToken.value)
 
-                if nextToken.type == TokenType.NUM:
-                    b = LiteralNumber(nextToken.value)
+                if lastOperation ==  "" and curentToken.value == "=":
+
+                    if curentToken.type == TokenType.NUM:
+                        a = LiteralNumber(curentToken.value)
+                    else:
+                        a = VarReading(curentToken.value)
+
+                if lastOperation ==  "":
+                    if prevToken.type == TokenType.NUM:
+                        a = LiteralNumber(prevToken.value)
+                    else:
+                        a = VarReading(prevToken.value)
+
+                    if nextToken.type == TokenType.NUM:
+                        b = LiteralNumber(nextToken.value)
+                    else:
+                        b = VarReading(nextToken.value)
+                    lastOperation=(BinaryOperation(token.value, b, a).toString(0))
+
+
                 else:
-                    b = VarReading(nextToken.value)
-                    
+                    if line[i].value == "=":
+                        t = VarDeclaration(line[i+2].value, VarReading(lastOperation))
+                        q = FunctionPrint(t)
+
+                        g = AbstractSyntaxTree(t, q)
+                        print(g.transpile())
+
+
+                    if nextToken.type == TokenType.NUM:
+                        b = LiteralNumber(nextToken.value)
+                    else:
+                        b = VarReading(nextToken.value)
+
                 # IL MANQUE LE FAIT QUE LE BINARY OPERATION PRECEDENT NE SOIT PAS REFILER DANS L'OPERATION SUIVANTE
 
-                print(BinaryOperation(token.value, b, a).toString(0))
+                print(lastOperation)
 
 
