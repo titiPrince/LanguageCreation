d1f70b67b5d38f98ef7f7cc175cbae3107e0aca4
vicous6 <vicous@free.fr>
2023-11-01 15:37:17 +0100

    push de commentaires

--- src/compiler/iteration-2/engine/Parser.py
+++ src/compiler/iteration-2/engine/Parser.py
@@ -6,10 +6,6 @@ NATIVES = [
 	"print"
 ]
 
-vartab = {}
-varId = 0
-equalCount = 0
-
 
 def isVarNameAvailable(element, vartab):
 	return element.value in vartab.keys()
@@ -18,12 +14,18 @@ def isVarNameAvailable(element, vartab):
 def isIdNative(element):
 	return element.value in NATIVES
 
-#   ID = 0
-#   OP = 1
-#   NUM = 2
-#   STR = 3 ["STR", truc]
-#   BOX = 4
-#   EOL = 5
+
+isIf = false
+isFor = false
+
+
+vartab = {}
+varId = 0
+equalCount = 0
+
+
+
+
 
 # if
 # (
@@ -46,22 +48,72 @@ def isIdNative(element):
 # for( i = 0 ; i < )
 
 def verifySyntax(tokens):
-	def scan(start, line):
-		global varId
-		global equalCount
 
-		isNotBegin = bool(start)
-		isBegin = not bool(start)
+# def areBracketsValid(tokens):
+# 	countBrackets = 0
+# 	for i in tokens:
+# 		if i.value== "{" :
+# 			countBrackets+=1
+# 		elif i.value== "}":
+# 			countBrackets-=1
+# 	return True if countBrackets == 0  else False
+# def areParenthesisValid(tokens):
+# 	countParentesises = 0
+# 	for i in tokens:
+# 		if i.value== "(" :
+# 			countParentesises+=1
+# 		elif i.value== ")":
+# 			countParentesises-=1
+# 	return True if countParentesises == 0  else False
 
-		if start != len(line) - 1:
-			currentEl = line[start]
-			nextEl = line[start + 1]
-			# print(currentEl)
-			# check si pas 2 "="
-			if currentEl.value == Symbol.ASSIGN:
-				equalCount += 1
 
-	pass
+# def scan(start, tokens):
+# 	global varId
+# 	global equalCount
+#
+# 	reset = True
+# 	for i in range(len(tokens)):
+# 		currentEl = tokens[start]
+# 		nextEl = tokens[start + 1]
+#
+		# # index 0 de l'iteration
+		# if i == 0 and (
+		# 	currentEl.type == TokenType.OP or
+		# 	currentEl.type == TokenType.BOX or
+		# 	currentEl.type == TokenType.STR or
+		# 	currentEl.type == TokenType.EOL or
+		# 	currentEl.type == TokenType.NUM
+		# ):
+		# 	return "tu fait pas d'effort dès le premier charactère"
+		#
+		# 	#  si la fonction native est en première place elle DOIT etre suivi de (
+		# elif currentEl.type == TokenType.ID and isIdNative(currentEl) and not nextEl.value == Symbol.PARS:
+		# 	return "Error Syntax: A native function must be follow by ':'"
+		#
+		#
+		# #
+		# # si la varible n'existe pas dans vartab et n'est pas en 1ere position
+		# elif currentEl.type == TokenType.ID and not isIdNative(currentEl) and not isVarNameAvailable(currentEl,vartab):
+		# #
+		# # 	return "Error Syntax: la varible '" + currentEl.value + "' n'est pas defini"
+		# #
+		# # # si la variable est en position 1 elle doit etre suivi d'un "=" (asignation ou delcaration)
+		# # elif currentEl.type == TokenType.ID and not isIdNative(currentEl) and nextEl.value != "=" and isBegin:
+		# # 	return "Error Syntax: la varible '" + currentEl.value + "' n'est pas suivi d'un '='"
+		#
+		# if(reset and (
+		# 	   currentEl.value == Symbol.ASSIGN or
+		# 	   currentEl.value == Symbol.ASSIGN
+		# 		)
+		# )
+
+
+
+
+
+
+
+
 
 def getAbstractTree(tokens):
 	for i, token in enumerate(tokens):
--- src/compiler/iteration-2/test.pp
+++ src/compiler/iteration-2/test.pp
@@ -1,6 +1,7 @@
 cha1ne = "Ceci est une chaine de caractere";
 autrechaine = "un exemple" + chaine + "test" + " waw";
-
+si i = 2 {;
+  si i = 2 { "2"};
 a = "a";
 b = "b" + a;
 
@@ -12,10 +13,8 @@ sinon
   a = "aba";
 }
 
-si 0 {
-  print("c'est 0");
-}
-etsi 1 {
+si 0 { print("c'est 0"); }
+etsi a == "ba" and 1 != 1 or 1 > 5 {
   print("c'est 1");
 }
 sinon {
